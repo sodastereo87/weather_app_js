5d2584a426850ebcc4391bf2f829318ce52114f9
sodastereo <f_er_@hotmail.com>
2019-09-12 01:02:09 -0400

    js added

--- scripts/app.js
+++ scripts/app.js
@@ -2,19 +2,23 @@ const cityForm = document.querySelector('form');
 
 const updateCity = async (city) => {
 
-const cityDets = await getCity(city);
-const weather = await getWeather(cityDets.key);
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+
+    return { cityDets, weather };
 
 };
 
 cityForm.addEventListener('submit', e => {
-// prevent default action
-e.preventDefault();
+    // prevent default action
+    e.preventDefault();
 
-// get city value
-const city = cityForm.city.value.trim();
-cityForm.reset();
+    // get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
 
-// update the ui with new city
-updateCity(city);
+    // update the ui with new city
+    updateCity(city)
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
 });
